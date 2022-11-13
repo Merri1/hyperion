a0594a722fad01827bb3854defe5486af4233f53
Merri1 <m121212@live.ie>
2022-11-13 16:30:07 +0000

    Helle World first commit

---
+++ Pathfinding.cpp
@@ -0,0 +1,12 @@
+//
+// Created by merri on 13/11/22.
+//
+
+#include "Pathfinding.h"
+#include <iostream>
+
+using namespace std;
+
+void Pathfinding::PrintSummary() {
+    cout << "This is the pathfinding class" << endl;
+}

c579ad3a565550a359ad7ad770db837321ab9459
Merri1 <K00252891@student.lit.ie>
2023-01-20 22:27:25 +0000

    Revert "Demo page with table"
    This reverts commit 5360df77e33a08ca932a91d4dec7fca5f26719f0.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDividerModule } from "@angular/material/divider";
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatTableModule} from "@angular/material/table";
-import {MatSortModule} from "@angular/material/sort";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatIconModule,
-    MatDividerModule,
-    MatSliderModule,
-    MatToolbarModule,
-    MatTableModule,
-    MatSortModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
